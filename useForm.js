766749732a4e047a694062a6ef3539e409093da4
jonathan gonzalez <you@example.com>
2022-07-26 18:01:19 -0300

    subiendo custom hooks

---
+++ useForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export const useForm = (initialForm={}) => {
+
+    const [formState, setFormState] = useState(initialForm)
+
+    const onInputChange=({target}) => {
+        const{name,value}=target;
+        setFormState({
+            ...formState,
+            [name]:value,
+            
+        }); 
+    }
+    const onReset=() => {
+        // const{name,value}=target;
+        setFormState(initialForm); 
+    }
+
+  return {
+    onInputChange,
+    ...formState,
+    formState,
+    onReset
+    
+  }
+}
